fe3c3f3893764bbce3e69f48cb5cd4423573d915
liyifan <1104140971@qq.com>
2025-01-02 17:01:35 +0800

    - @PrintInfo

--- design-pattern/src/main/java/org/example/utils/AnnotationHelper.java
+++ design-pattern/src/main/java/org/example/utils/AnnotationHelper.java
@@ -1,7 +1,24 @@
 package org.example.utils;
 
-public class AnnotationHelper {
-  public void PrintInfoAnnotation(Object handler) {
+import org.example.annotations.PrintInfo;
 
+import java.lang.reflect.Field;
+
+public class AnnotationHelper {
+  public void printInfoAnnotation(Object handler) {
+    Class<?> clazz = handler.getClass();
+    if (clazz.isAnnotationPresent(PrintInfo.class)) {
+      // Process class-level annotation
+      PrintInfo annotation = clazz.getAnnotation(PrintInfo.class);
+      System.out.println("Class annotation info: " + annotation.value());
+    }
+
+    // Optionally process field-level annotations
+    for (Field field : clazz.getDeclaredFields()) {
+      if (field.isAnnotationPresent(PrintInfo.class)) {
+        PrintInfo fieldAnnotation = field.getAnnotation(PrintInfo.class);
+        System.out.println("Field annotation info: " + fieldAnnotation.value());
+      }
+    }
   }
 }
