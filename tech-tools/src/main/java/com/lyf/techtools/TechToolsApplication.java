67810d385b601c37dde58c093d03b209d467499e
yifan.li <liyifan_official@163.com>
2025-10-28 15:11:45 +0800

    - Fix pom errors

---
+++ tech-tools/src/main/java/com/lyf/techtools/TechToolsApplication.java
@@ -0,0 +1,13 @@
+package com.lyf.techtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechToolsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TechToolsApplication.class, args);
+  }
+
+}
---
+++ tech-tools/src/test/java/com/lyf/techtools/TechToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyf.techtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechToolsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
